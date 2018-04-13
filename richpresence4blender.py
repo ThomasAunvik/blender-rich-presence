32600dab384c951dac9577e51a687f64c193722d
AlexApps99 <30854646+AlexApps99@users.noreply.github.com>
2018-04-14 10:43:46 +1200

    Added import bpy, replaced spaces with tabs and added register()

--- richpresence4blender.py
+++ richpresence4blender.py
@@ -1,18 +1,24 @@
 bl_info = {
-    "name": "Discord Rich Presence",
-    "description": "Adds Rich Presence support to Blender",
-    "author": "@AlexApps#9295, @lvxejay#9771",
-    "version": (0, 1),
-    "blender": (2, 79, 0),
-    "location": "",
-    "warning": "Extremely Unstable - Still in development!",
-    "wiki_url": "https://github.com/AlexApps99/blender-rich-presence/wiki",
-    "tracker_url": "https://github.com/AlexApps99/blender-rich-presence/issues",
-    "support": "COMMUNITY",
-    "category": "System"
-    }
+	"name": "Discord Rich Presence",
+	"description": "Adds Rich Presence support to Blender",
+	"author": "@AlexApps#9295, @lvxejay#9771",
+	"version": (0, 1),
+	"blender": (2, 79, 0),
+	"location": "",
+	"warning": "Extremely Unstable - Still in development!",
+	"wiki_url": "https://github.com/AlexApps99/blender-rich-presence/wiki",
+	"tracker_url": "https://github.com/AlexApps99/blender-rich-presence/issues",
+	"support": "COMMUNITY",
+	"category": "System"
+	}
 import rpc
 import time
+import bpy
+
+def register():
+	print('Placeholder')
+def unregister():
+	print('Placeholder')
 
 client_id = '434079082339106827'
 rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)
@@ -21,18 +27,18 @@ start_time = time.time()
 filename = 'test.blend'
 version_no = '2.79'
 while True:
-    activity = {
-            "details": "Using Blender " + version_no,
-            "state": "Working on " + filename,
-            "timestamps": {
-                "start": start_time
-            },
-            "assets": {
-                "small_text": "Blender " + version_no,
-                "small_image": "blender_logo",
-                "large_text": filename,
-                "large_image": "pretty_render"
-            }
-        }
-    rpc_obj.set_activity(activity)
-    time.sleep(30)
+	activity = {
+		"details": "Using Blender " + version_no,
+		"state": "Working on " + filename,
+		"timestamps": {
+			"start": start_time
+		},
+		"assets": {
+			"small_text": "Blender " + version_no,
+			"small_image": "blender_logo",
+			"large_text": filename,
+			"large_image": "pretty_render"
+		}
+	}
+	rpc_obj.set_activity(activity)
+	time.sleep(30)
